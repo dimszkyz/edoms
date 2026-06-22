c1d1a17b11b1c195c1a0521193618f0abeea5e80
Dimas Rizky <schooldims@gmail.com>
2026-06-22 10:38:26 +0700

    Route public EDOM through homepage

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,8 @@
 use App\Http\Controllers\EdomPublicController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [EdomPublicController::class, 'index'])->name('edom.home');
+Route::post('/', [EdomPublicController::class, 'submitFromHome'])->name('edom.home.submit');
 
 Route::get('/edom/{edom}/isi', [EdomPublicController::class, 'show'])->name('edom.fill');
 Route::post('/edom/{edom}/isi', [EdomPublicController::class, 'submit'])->name('edom.submit');
