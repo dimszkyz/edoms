4159e0d7c5bb6ee168cba72db793140a19697817
Dimas Rizky <schooldims@gmail.com>
2026-06-22 13:20:53 +0700

    Add public master Blade layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>@yield('title', 'EDOM Universitas Ngudi Waluyo')</title>
+
+    @yield('styles')
+</head>
+<body>
+    @include('component.header')
+
+    @yield('content')
+
+    @include('component.footer')
+
+    @yield('scripts')
+</body>
+</html>
