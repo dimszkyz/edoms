70766832e43952641a567404aad82758dce16d17
Dimas Rizky <schooldims@gmail.com>
2026-06-22 10:20:18 +0700

    Add public EDOM input routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\EdomPublicController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/edom/{edom}/isi', [EdomPublicController::class, 'show'])->name('edom.fill');
+Route::post('/edom/{edom}/isi', [EdomPublicController::class, 'submit'])->name('edom.submit');
