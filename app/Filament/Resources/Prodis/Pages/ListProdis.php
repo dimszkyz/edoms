da72f105f888f403296d545cc86c9034fe00d0d9
Dimas Rizky <schooldims@gmail.com>
2026-06-22 15:45:05 +0700

    Add sync action to Prodi list page

--- app/Filament/Resources/Prodis/Pages/ListProdis.php
+++ app/Filament/Resources/Prodis/Pages/ListProdis.php
@@ -3,8 +3,11 @@
 namespace App\Filament\Resources\Prodis\Pages;
 
 use App\Filament\Resources\Prodis\ProdiResource;
-use Filament\Actions\CreateAction;
+use App\Services\UnwProgramStudiSyncService;
+use Filament\Actions\Action;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ListRecords;
+use Throwable;
 
 class ListProdis extends ListRecords
 {
@@ -13,7 +16,31 @@ class ListProdis extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            CreateAction::make(),
+            Action::make('syncProgramStudi')
+                ->label('Sinkronisasi Prodi')
+                ->icon('heroicon-o-arrow-path')
+                ->color('primary')
+                ->requiresConfirmation()
+                ->modalHeading('Sinkronisasi Prodi dari API UNW')
+                ->modalDescription('Data prodi akan diambil dari API resmi UNW. Data yang sudah ada akan diperbarui berdasarkan ID prodi dari API.')
+                ->modalSubmitActionLabel('Mulai Sinkronisasi')
+                ->action(function (): void {
+                    try {
+                        $result = app(UnwProgramStudiSyncService::class)->sync();
+
+                        Notification::make()
+                            ->title('Sinkronisasi Prodi berhasil')
+                            ->body("{$result['created']} data baru, {$result['updated']} data diperbarui, {$result['skipped']} data dilewati dari total {$result['total']} data API.")
+                            ->success()
+                            ->send();
+                    } catch (Throwable $exception) {
+                        Notification::make()
+                            ->title('Sinkronisasi Prodi gagal')
+                            ->body($exception->getMessage())
+                            ->danger()
+                            ->send();
+                    }
+                }),
         ];
     }
 }
